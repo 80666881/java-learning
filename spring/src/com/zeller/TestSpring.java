0a95863c00bff12900067319884d1440de3fef2e
zeller <87192329@163.com>
2019-06-10 17:36:35 +0800

    Spring注解方式测试

--- spring/src/com/zeller/TestSpring.java
+++ spring/src/com/zeller/TestSpring.java
@@ -1,17 +1,19 @@
 package com.zeller;
 
-import com.zeller.service.ProductService;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
 public class TestSpring {
+    @Autowired
+    Category c;
 
-    public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[] { "applicationContext.xml" });
-
-        ProductService s = (ProductService) context.getBean("s");
-        s.doSomeService();
-        s.doSomeService2();
+    @Test
+    public void test(){
+        System.out.println(c.getCat());
     }
 }
