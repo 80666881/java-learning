fa247a1398114877078ccfeea12d6d6da222e301
zeller <87192329@163.com>
2019-06-11 14:51:33 +0800

    hibernate使用Criteria查询

--- hibernate/src/com/zeller/TestHibernate.java
+++ hibernate/src/com/zeller/TestHibernate.java
@@ -2,6 +2,8 @@ package com.zeller;
 import java.util.List;
 
 import org.hibernate.Query;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,19 +12,24 @@ public class TestHibernate {
     public static void main(String[] args) {
         SessionFactory sf = new Configuration().configure().buildSessionFactory();
         /*
-        *   1. 首先根据hql创建一个Query对象
-            2. 设置参数(和基1的PreparedStatement不一样，Query是基0的)
-            3. 通过Query对象的list()方法即返回查询的结果了。
-            注： 使用hql的时候，用的是类名Product,而不是表名product_
-            注： 使用hql的时候，不需要在前面加 select *
+        *   使用Criteria 查询数据
+                1. 通过session的createCriteria创建一个Criteria 对象
+                2. Criteria.add 增加约束。 在本例中增加一个对name的模糊查询(like)
+                3. 调用list()方法返回查询结果的集合
         *
         * */
         Session s = sf.openSession();
         s.beginTransaction();
         String name = "iphone";
-        Query q =s.createQuery("from Product p where p.name like ?");
-        q.setString(0, "%"+name+"%");
-        List<Product> ps= q.list();
+//        Query q =s.createQuery("from Product p where p.name like ?");
+//        q.setString(0, "%"+name+"%");
+//        List<Product> ps= q.list();
+//        for (Product p : ps) {
+//            System.out.println(p.getName());
+//        }
+        Criteria c= s.createCriteria(Product.class);
+        c.add(Restrictions.like("name", "%"+name+"%"));
+        List<Product> ps = c.list();
         for (Product p : ps) {
             System.out.println(p.getName());
         }
