e4ea7cb5f045fe1c939553b877f91b65c99dfdd8
zeller <87192329@163.com>
2019-06-11 11:51:08 +0800

    hibernate状态

--- hibernate/src/com/zeller/TestHibernate.java
+++ hibernate/src/com/zeller/TestHibernate.java
@@ -7,25 +7,33 @@ import org.hibernate.cfg.Configuration;
 public class TestHibernate {
     public static void main(String[] args) {
 
+//        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+//
+//        Session s = sf.openSession();
+//        s.beginTransaction();
+//
+//        for (int i = 0; i < 10; i++) {
+//            Product p = new Product();
+//            p.setName("iphone"+i);
+//            p.setPrice(i);
+//            s.save(p);
+//        }
+//
+//        s.getTransaction().commit();
+//        s.close();
+//        sf.close();
         SessionFactory sf = new Configuration().configure().buildSessionFactory();
 
         Session s = sf.openSession();
         s.beginTransaction();
-
-//        Product p = new Product();
-//        p.setName("iphone7");
-//        p.setPrice(7000);
-//        s.save(p);
-
-        for (int i = 0; i < 10; i++) {
-            Product p = new Product();
-            p.setName("iphone"+i);
-            p.setPrice(i);
-            s.save(p);
-        }
-
+        Product p = new Product();
+        p.setName("p1");
+        System.out.println("此时p是瞬时状态");
+        s.save(p);
+        System.out.println("此时p是持久状态");
         s.getTransaction().commit();
         s.close();
+        System.out.println("此时p是脱管状态");
         sf.close();
     }
 
