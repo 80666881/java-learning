7d72af95757ed69d5a0d37af0c51b97194dc4379
zeller <87192329@163.com>
2019-06-11 10:05:42 +0800

    struts使用session

--- struts/out/artifacts/struts_war_exploded/show.jsp
+++ struts/out/artifacts/struts_war_exploded/show.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8" isELIgnored="false"%>
 ${product.name}
-
+${name}
 <div style="color:red;">${product.id}</div>
--- struts/src/com/how2java/action/ProductAction.java
+++ struts/src/com/how2java/action/ProductAction.java
@@ -1,11 +1,25 @@
 package com.how2java.action;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+
 import com.how2java.bean.Product;
+import com.opensymphony.xwork2.ActionContext;
+//import com.opensymphony.xwork2.ActionSupport;
 
 public class ProductAction {
 	private Product product;
 
 	public String show() {
+		HttpServletRequest request = ServletActionContext.getRequest();
+		HttpServletResponse response = ServletActionContext.getResponse();
+
+		System.out.println("request:\t" + request);
+		System.out.println("response:\t" + response);
 		product = new Product();
 		product.setName("iphone7");
 		product.setId(23);
@@ -21,6 +35,8 @@ public class ProductAction {
 	}
 
 	public String add(){
+		Map m = ActionContext.getContext().getSession();
+		m.put("name", product.getName());
 		System.out.println("product.name:"+product.getName());
 		return "showJsp";
 	}
