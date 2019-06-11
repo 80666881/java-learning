21bad693edad75042f8161fa02f1bd26910e8353
zeller <87192329@163.com>
2019-06-11 14:48:14 +0800

    hibernate使用hql查询

--- hibernate/src/com/zeller/TestHibernate.java
+++ hibernate/src/com/zeller/TestHibernate.java
@@ -1,51 +1,31 @@
 package com.zeller;
+import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class TestHibernate {
     public static void main(String[] args) {
-
-//        SessionFactory sf = new Configuration().configure().buildSessionFactory();
-//
-//        Session s = sf.openSession();
-//        s.beginTransaction();
-//
-
-//        s.getTransaction().commit();
-//        s.close();
-//        sf.close();
-
         SessionFactory sf = new Configuration().configure().buildSessionFactory();
-
+        /*
+        *   1. 首先根据hql创建一个Query对象
+            2. 设置参数(和基1的PreparedStatement不一样，Query是基0的)
+            3. 通过Query对象的list()方法即返回查询的结果了。
+            注： 使用hql的时候，用的是类名Product,而不是表名product_
+            注： 使用hql的时候，不需要在前面加 select *
+        *
+        * */
         Session s = sf.openSession();
         s.beginTransaction();
-
-        //增
-//        for (int i = 0; i < 10; i++) {
-//            Product p = new Product();
-//            p.setName("iphone"+i);
-//            p.setPrice(i);
-//            s.save(p);
-//        }
-//
-//        Product p =(Product) s.get(Product.class, 6);
-//        删
-//        s.delete(p);
-
-        //改
-//        p.setName("iphone-modified");
-//        s.update(p);
-
-
-        //查get，非延迟，立即执行sql，拿不到返回null
-//        Product p =(Product) s.get(Product.class, 6);
-
-        //查load，延迟，返回属性才立即执行sql，拿不到返回null
-        Product p =(Product) s.load(Product.class, 6);
-
-        System.out.println("id=6的产品名称是: "+p.getName());
+        String name = "iphone";
+        Query q =s.createQuery("from Product p where p.name like ?");
+        q.setString(0, "%"+name+"%");
+        List<Product> ps= q.list();
+        for (Product p : ps) {
+            System.out.println(p.getName());
+        }
 
         s.getTransaction().commit();
         s.close();
