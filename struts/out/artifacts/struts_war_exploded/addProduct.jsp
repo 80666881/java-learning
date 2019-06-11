4525834a65b2b3e9d0bdc0ea943c91006a78a847
zeller <87192329@163.com>
2019-06-11 10:32:18 +0800

    strut标签

--- struts/out/artifacts/struts_war_exploded/addProduct.jsp
+++ struts/out/artifacts/struts_war_exploded/addProduct.jsp
@@ -6,15 +6,18 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+
 <html>
-<head>
-    <title>add product</title>
-</head>
+
 <body>
-<form action="addProduct">
-    <input type="text" name="product.name">
-    <br/>
-    <input type="submit" value="submit">
-</form>
+
+<s:form action="addProduct">
+
+    <s:textfield name="product.name" label="product name" />
+    <s:submit value="Submit" />
+
+</s:form>
+
 </body>
 </html>
--- struts/src/com/how2java/action/ProductAction.java
+++ struts/src/com/how2java/action/ProductAction.java
@@ -1,5 +1,6 @@
 package com.how2java.action;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,11 +10,18 @@ import org.apache.struts2.ServletActionContext;
 
 import com.how2java.bean.Product;
 import com.opensymphony.xwork2.ActionContext;
-//import com.opensymphony.xwork2.ActionSupport;
 
 public class ProductAction {
 	private Product product;
+	private List<Product> products;
 
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
 	public String show() {
 		HttpServletRequest request = ServletActionContext.getRequest();
 		HttpServletResponse response = ServletActionContext.getResponse();
@@ -25,7 +33,22 @@ public class ProductAction {
 		product.setId(23);
 		return "showJsp";
 	}
-
+	public String list() {
+		products=new ArrayList();
+		Product p1 = new Product();
+		p1.setId(1);
+		p1.setName("product1");
+		Product p2 = new Product();
+		p2.setId(2);
+		p2.setName("product2");
+		Product p3 = new Product();
+		p3.setId(3);
+		p3.setName("product3");
+		products.add(p1);
+		products.add(p2);
+		products.add(p3);
+		return "list";
+	}
 	public Product getProduct() {
 		return product;
 	}
