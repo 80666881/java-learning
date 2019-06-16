208009452571bf2bb8cc293d6616e354d4b1fa19
zeller <87192329@163.com>
2019-06-16 08:51:08 +0800

    hibernate级联

--- hibernate/src/com/zeller/TestHibernate.java
+++ hibernate/src/com/zeller/TestHibernate.java
@@ -13,20 +13,31 @@ import org.hibernate.cfg.Configuration;
 public class TestHibernate {
     public static void main(String[] args) {
         /*
-        * 关系延迟加载
-        * 延迟加载又叫lazyload，在one-many many-many的时候都可以使用关系的延迟加载
-        * 1.修改配置文件 Category.hbm.xml
-        * 2.执行25行的时候，只会查询Category表的信息，不会查询product_表
-            只有在执行28行，通过category取products的时候，才会进行对product_表的查询
+        * 什么是级联？ 简单的说，没有配置级联的时候，删除分类，其对应的产品不会被删除。
+        * 但是如果配置了恰当的级联，那么删除分类的时候，其对应的产品都会被删除掉。
+        *
+        *   包括上一步说的删除用得级联，级联有4种类型：
+                all：所有操作都执行级联操作；
+                none：所有操作都不执行级联操作；
+                delete：删除时执行级联操作；
+                save-update：保存和更新时执行级联操作；
+            级联通常用在one-many和many-to-many上，几乎不用在many-one上。
         * */
         SessionFactory sf = new Configuration().configure().buildSessionFactory();
         Session s = sf.openSession();
         s.beginTransaction();
-        Category c = (Category) s.get(Category.class, 1);
+        //级联delete
+//        Category c = (Category) s.get(Category.class, 2);
+//        s.delete(c);
 
-        System.out.println("log1");
-        System.out.println(c.getProducts());
-        System.out.println("log2");
+        //级联update
+        Category c = (Category) s.get(Category.class, 3);
+        Product p1 = new Product();
+        p1.setName("product_501");
+        Product p2 = new Product();
+        p2.setName("product_502");
+        Product p3 = new Product();
+        p3.setName("product_503");
 
         s.getTransaction().commit();
         s.close();
