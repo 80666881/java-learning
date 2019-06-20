56ce3283bf4c152189632a07932044bbbeb0e212
zeller <87192329@163.com>
2019-06-20 23:38:03 +0800

    spring mvc注解配置视图

--- springmvc/src/controller/IndexController.java
+++ springmvc/src/controller/IndexController.java
@@ -4,22 +4,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+//import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-public class IndexController implements Controller {
+@Controller
+public class IndexController {
+	@RequestMapping("/index")
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-		/*
-		如果代码写成这样，就表示跳转到页面 index.jsp
-		new ModelAndView("index.jsp");
-
-		所谓的视图定位，指的是代码还是写成这样，但是会跳转到 /WEB-INF/page/index.jsp
-		new ModelAndView("index");
-		*/
-
-//		ModelAndView mav = new ModelAndView("index.jsp");
 		ModelAndView mav = new ModelAndView("index");
-
 		mav.addObject("message", "Hello Spring MVC");
 		return mav;
 	}
