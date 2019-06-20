34899e642968e1e42cc854922f632188384bdc94
zeller <87192329@163.com>
2019-06-20 23:30:57 +0800

    spring mvc视图定位

--- springmvc/src/controller/IndexController.java
+++ springmvc/src/controller/IndexController.java
@@ -8,7 +8,18 @@ import org.springframework.web.servlet.mvc.Controller;
 
 public class IndexController implements Controller {
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ModelAndView mav = new ModelAndView("index.jsp");
+
+		/*
+		如果代码写成这样，就表示跳转到页面 index.jsp
+		new ModelAndView("index.jsp");
+
+		所谓的视图定位，指的是代码还是写成这样，但是会跳转到 /WEB-INF/page/index.jsp
+		new ModelAndView("index");
+		*/
+
+//		ModelAndView mav = new ModelAndView("index.jsp");
+		ModelAndView mav = new ModelAndView("index");
+
 		mav.addObject("message", "Hello Spring MVC");
 		return mav;
 	}
