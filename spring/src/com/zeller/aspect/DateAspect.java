0957db99447cfae18a45cd0299eb0c5b74723e44
zeller <87192329@163.com>
2019-06-20 22:54:58 +0800

    注解方式使用apo

--- spring/src/com/zeller/aspect/DateAspect.java
+++ spring/src/com/zeller/aspect/DateAspect.java
@@ -1,9 +1,13 @@
 package com.zeller.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
-
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 import java.util.Date;
-
+@Aspect
+@Component
 public class DateAspect {
+    @Around(value = "execution(* com.zeller.service.ProductService.*(..))")
     public Object logDate(ProceedingJoinPoint joinPoint) throws Throwable {
         System.out.println("start date:" + new Date().toString());
         Object object = joinPoint.proceed();
