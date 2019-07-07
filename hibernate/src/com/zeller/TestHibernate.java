ea03967b9112fa85c6f3635703067d61f769b0f3
zeller <87192329@163.com>
2019-07-07 22:54:08 +0800

    分页查询

--- hibernate/src/com/zeller/TestHibernate.java
+++ hibernate/src/com/zeller/TestHibernate.java
@@ -11,54 +11,31 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-/*
-    什么是级联？ 简单的说，没有配置级联的时候，删除分类，其对应的产品不会被删除。
-    但是如果配置了恰当的级联，那么删除分类的时候，其对应的产品都会被删除掉。
 
-*   包括上一步说的删除用得级联，级联有4种类型：
-    all：所有操作都执行级联操作；
-    none：所有操作都不执行级联操作；
-    delete：删除时执行级联操作；
-    save-update：保存和更新时执行级联操作；
-    级联通常用在one-many和many-to-many上，几乎不用在many-one上。
-*
-* */
 public class TestHibernate {
     public static void main(String[] args) {
-
-        //delete级联
-//        SessionFactory sf = new Configuration().configure().buildSessionFactory();
-//        Session s = sf.openSession();
-//        s.beginTransaction();
-//        //删除Category的id为5的分类，并且删除关联的产品
-//        Category c = (Category) s.get(Category.class, 5);
-//        s.delete(c);
-//        s.getTransaction().commit();
-//        s.close();
-//        sf.close();
-
-
-        //save-update级联
+        //使用Criteria进行分页查询
         SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
         Session s = sf.openSession();
         s.beginTransaction();
-        Category c = (Category) s.get(Category.class, 4);
 
-        Product p1 = new Product();
-        p1.setName("product_501");
-        Product p2 = new Product();
-        p2.setName("product_502");
-        Product p3 = new Product();
-        p3.setName("product_503");
+        String name = "iphone";
 
-        c.getProducts().add(p1);
-        c.getProducts().add(p2);
-        c.getProducts().add(p3);
+        Criteria c= s.createCriteria(Product.class);
+        c.add(Restrictions.like("name", "%"+name+"%"));
+        c.setFirstResult(2);//从第3条数据开始
+        c.setMaxResults(5);//查询5条数据
+
+        List<Product> ps = c.list();
+        for (Product p : ps) {
+            System.out.println(p.getName());
+
+        }
 
         s.getTransaction().commit();
         s.close();
         sf.close();
-
     }
 
 }
