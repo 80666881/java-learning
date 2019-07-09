07d5e884a7eb24969f2bbafc62caba9304ab13c1
zeller <87192329@163.com>
2019-07-09 19:55:48 +0800

    分页查询

--- spring_hibernate/src/com/how2java/test/TestSpring.java
+++ spring_hibernate/src/com/how2java/test/TestSpring.java
@@ -1,7 +1,7 @@
 package com.how2java.test;
  
 import java.util.List;
-
+import org.hibernate.criterion.DetachedCriteria;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,21 +15,29 @@ public class TestSpring {
         ApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[] { "applicationContext.xml" });
         CategoryDAO dao = (CategoryDAO) context.getBean("dao");
-        Category c = new Category();
-        c.setName("category yyy");
-        
-        //增加
-        dao.save(c);
-        
-        //获取
-        Category c2 = dao.get(Category.class, 1);
-        
-        //修改
-        c2.setName("category zzz");
-        dao.update(c2);
-        
-        //删除
-        dao.delete(c2);
+//        Category c = new Category();
+//        c.setName("category yyy");
+//
+//        //增加
+//        dao.save(c);
+//
+//        //获取
+//        Category c2 = dao.get(Category.class, 1);
+//
+//        //修改
+//        c2.setName("category zzz");
+//        dao.update(c2);
+//
+//        //删除
+//        dao.delete(c2);
+
+
+        //使用DetachedCriteria进行分页查询
+        DetachedCriteria dc = DetachedCriteria.forClass(Category.class);
+        int start =5;//从多少开始查询
+        int count =10;//每页显示数量
+        List<Category> cs= dao.findByCriteria(dc,start,count);
+        System.out.println(cs);
         
         
 
