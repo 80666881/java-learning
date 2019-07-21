7603207d42af481e7f1057f6b0d4200491744131
zeller <87192329@163.com>
2019-07-21 16:20:10 +0800

    session count

---
+++ springmvc/out/artifacts/springmvc_war_exploded/WEB-INF/page/check.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zeller
+  Date: 2019-07-21
+  Time: 11:32
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>check</title>
+</head>
+<body>
+session中记录的访问次数：${count}
+</body>
+</html>
--- springmvc/src/controller/IndexController.java
+++ springmvc/src/controller/IndexController.java
@@ -2,6 +2,7 @@ package controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,23 @@ public class IndexController {
 	@RequestMapping("/jump")
 	public ModelAndView jump() {
 		ModelAndView mav = new ModelAndView("redirect:/index");
+		return mav;
+	}
+
+	/*
+	* 	映射 /check 到方法check()
+		为方法check()提供参数HttpSession session，这样就可以在方法体中使用session了
+		接下来的逻辑就是每次访问为session中的count+1.
+		最后跳转到check.jsp页面
+	* */
+	@RequestMapping("/check")
+	public ModelAndView check(HttpSession session) {
+		Integer i = (Integer) session.getAttribute("count");
+		if (i == null)
+			i = 0;
+		i++;
+		session.setAttribute("count", i);
+		ModelAndView mav = new ModelAndView("check");
 		return mav;
 	}
 }
