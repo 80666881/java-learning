e05ce88f7ec067b775ef3fd62d1580a5bc8ee12b
zeller <87192329@163.com>
2019-07-21 11:28:34 +0800

    客户端跳转

--- springmvc/src/controller/IndexController.java
+++ springmvc/src/controller/IndexController.java
@@ -18,4 +18,10 @@ public class IndexController {
 		mav.addObject("message", "Hello Spring MVC");
 		return mav;
 	}
+
+	@RequestMapping("/jump")
+	public ModelAndView jump() {
+		ModelAndView mav = new ModelAndView("redirect:/index");
+		return mav;
+	}
 }
