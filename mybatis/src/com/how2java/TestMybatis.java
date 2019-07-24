d888cdf7b81e25ad46f3fa8404b82e63199b9e62
zeller <87192329@163.com>
2019-07-24 17:37:25 +0800

    一对多

--- mybatis/src/com/how2java/TestMybatis.java
+++ mybatis/src/com/how2java/TestMybatis.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.how2java.pojo.Product;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -19,36 +20,15 @@ public class TestMybatis {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession session=sqlSessionFactory.openSession();
+        SqlSession session = sqlSessionFactory.openSession();
 
-//        Category c = new Category();
-//        c.setName("新增加的Category");
-//        session.insert("addCategory",c);
-        //        listAll(session);
-
-//        List<Category> cs = session.selectList("listCategoryByName","1");
-//        for (Category c : cs) {
-//            System.out.println(c.getName());
-//        }
-
-        Map<String,Object> params = new HashMap<>();
-        params.put("id", 2);
-        params.put("name", "at");
-
-        List<Category> cs = session.selectList("listCategoryByIdAndName",params);
-        for (Category c : cs) {
-            System.out.println(c.getName());
-        }
-
-
-        session.commit();
-        session.close();
-
-    }
-    private static void listAll(SqlSession session) {
         List<Category> cs = session.selectList("listCategory");
         for (Category c : cs) {
-            System.out.println(c.getName());
+            System.out.println(c);
+            List<Product> ps = c.getProducts();
+            for (Product p : ps) {
+                System.out.println("\t"+p);
+            }
         }
     }
 }
