56800449236816548ed5faaca63418fc01458240
zeller <87192329@163.com>
2019-09-01 21:18:14 +0800

    mybatis多对多

--- mybatis/src/com/how2java/TestMybatis.java
+++ mybatis/src/com/how2java/TestMybatis.java
@@ -9,6 +9,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import com.how2java.pojo.Order;
+import com.how2java.pojo.OrderItem;
 import com.how2java.pojo.Product;
 
 public class TestMybatis {
@@ -19,13 +21,21 @@ public class TestMybatis {
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession session = sqlSessionFactory.openSession();
 
-        List<Product> ps = session.selectList("listProduct");
-        for (Product p : ps) {
-            System.out.println(p+" 对应的分类是 \t "+ p.getCategory());
-        }
+        listOrder(session);
 
         session.commit();
         session.close();
 
+    }
+
+    private static void listOrder(SqlSession session) {
+        List<Order> os = session.selectList("listOrder");
+        for (Order o : os) {
+            System.out.println(o.getCode());
+            List<OrderItem> ois= o.getOrderItems();
+            for (OrderItem oi : ois) {
+                System.out.format("\t%s\t%f\t%d%n", oi.getProduct().getName(),oi.getProduct().getPrice(),oi.getNumber());
+            }
+        }
     }
 }
